aed6591bf02bb0ce50f97c9d4a0ac9664552b08a
Ronny <ronnychamba96@gmail.com>
2021-11-17 22:12:00 -0500

    add tipo-transporte.php

---
+++ php/list-tipo-transporte.php
@@ -0,0 +1,62 @@
+<?php
+  include('../conexion.php');
+  $sql=("SELECT * FROM TIPO_TRANSPORTE");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Tipo Transporte</title>
+  <link rel="stylesheet" href="../style.css">
+</head>
+<body>
+  
+  <div class="container">
+    <section class="content content--header">
+      <header class="header">
+        <h1 class="title">Tipos de Transportes Registrados</h1>
+      </header>
+    </section>
+    <section class="content content--main">
+
+    <?php
+      if($resultado= mysqli_query($conexion, $sql))
+   {
+    ?>
+    <table class="table">
+
+    <thead class="table-head">
+      <tr>
+         <th class="table-celd table-celd--th">C&oacute;dula</th>
+         <th class="table-celd table-celd--th">Tipo</th>
+         <th class="table-celd table-celd--th">N&uacute;mero Asientos</th>
+      </tr>
+    </thead>
+    <tbody class="table-body">
+     <?php
+          while($fila=mysqli_fetch_assoc($resultado))
+       {
+      ?>
+
+      <tr>
+        <td class="table-celd table-celd-td"> <?php echo "".$fila['TIPO_TRANSPORTE_COD']?>   </td>
+        <td class="table-celd table-celd-td"> <?php echo "".$fila['TIPO_TRANSPORTE_NOM']?>   </td>
+        <td class="table-celd table-celd-td"> <?php echo "".$fila['TIPO_TRANSPORTE_ASI']?>   </td>
+      </tr>
+      <?php }?>
+
+
+    </tbody>
+    </table>
+    <?php }else{
+
+      echo "<p>Surgio un error al consultar los datos</p>";
+    } ?>
+    <p> <a href="/index.html" class="link link--return">Regresar al menú</a></p>
+    </section>
+  </div>
+</body>
+</html>
